81e3926c4053932491f2c0e0fd419192b3270793
Chase Childers <chasechilders12@gmail.com>
2023-03-17 10:33:12 -0400

    added todo form component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import './components/TodoForm'
+import TodoForm from './components/TodoForm';
 
 function App() {
   return (
     <div className="App">
-      <h1>Productiv :</h1>
+      <h1>Productiv Version 1.0:</h1>
+      <div className='components'>
+        <TodoForm/>
+      </div>
     </div>
   );
 }
